9ee41f21af81b7013d61bfe4603d7a378cf9a300
joindevopscloud <info@joindevops.com>
2024-01-29 08:54:28 +0530

    daws-76s

--- 10-cdn/main.tf
+++ 10-cdn/main.tf
@@ -11,18 +11,36 @@ resource "aws_cloudfront_distribution" "daws76s" {
       origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/images/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "web-${var.environment}.${var.zone_name}"
+
+    cache_policy_id = data.aws_cloudfront_cache_policy.cache.id
+    compress               = true
+    viewer_protocol_policy = "https-only"
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/static/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "web-${var.environment}.${var.zone_name}"
+
+    cache_policy_id = data.aws_cloudfront_cache_policy.cache.id
+    compress               = true
+    viewer_protocol_policy = "https-only"
+  }
+
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods         = ["GET", "HEAD", "OPTIONS"]
     target_origin_id       = "web-${var.environment}.${var.zone_name}"
     viewer_protocol_policy = "https-only"
-    forwarded_values {
-      headers      = []
-      query_string = true
-      cookies {
-        forward = "all"
-      }
-    }
+    cache_policy_id = data.aws_cloudfront_cache_policy.no_cache.id
   }
   restrictions {
     geo_restriction {
