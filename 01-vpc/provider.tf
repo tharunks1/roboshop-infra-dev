c3cae2613aa47e70a97e0ff6d58bebee091a84ce
SRINIVAS_THARUN <88927252+tharunks1@users.noreply.github.com>
2024-10-29 20:33:29 +0530

    Update provider.tf

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "daws76s-state-dev"
+    bucket = "daws76s-state-dev1"
     key    = "vpc"
     region = "us-east-1"
     dynamodb_table = "daws76s-locking-dev"
